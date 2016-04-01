bc1373bcaf346f2ba054d9ae8a41479ce4343375
Eric Blade <blade.eric@gmail.com>
2016-04-01 00:29:15 +0000

    make sure lib output directory exists in install-headers

--- support/install-headers.mk
+++ support/install-headers.mk
@@ -5,6 +5,8 @@ $(call DL,BUILD_SUPPORT)
 staging/armv7/usr/include/.staged: staging/$(WOCE_ARCH)/.unpacked $(BUILD_SUPPORT_DL)
 	-rm -rf staging/armv7/usr/include
 	mkdir -p staging/armv7/usr/include
+	-rm -rf staging/armv7/usr/lib
+	mkdir -p staging/armv7/usr/lib
 	tar -C staging/armv7/usr/include --strip=4 --wildcards -axf $(BUILD_SUPPORT_DL) 'woce-build-support-[a-f0-9]*/staging/arm-none-linux-gnueabi/include'
 	tar -C staging/armv7/usr/lib --strip=4 --wildcards -axf $(BUILD_SUPPORT_DL) 'woce-build-support-[a-f0-9]*/staging/arm-none-linux-gnueabi/lib'
 	ln -s libsqlite3.so.0 staging/armv7/usr/lib/libsqlite3.so
